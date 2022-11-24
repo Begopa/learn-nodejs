66f07bba4d9040c2ed92a3fb053daffcca3e592c
philip <philip0813@naver.com>
2022-11-24 12:28:58 +0000

    added passport/index.js

--- chapter9/nordbird/passport/index.js
+++ chapter9/nordbird/passport/index.js
@@ -1,1 +1,18 @@
-const local = require('./localStrategy');
+eredconst local = require('./localStrategy');
+const kakao = require('./kakaoStrategy');
+const { User } = require('../models');
+
+module.exports = (passport) => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    User.fin({ wh: { id } })
+      .then(user => done(null, user))
+      .catch(err => done(err));
+  });
+  
+  local(passport);
+  kakao(passport);
+};
